4fce30a8130612874fec4ea35c0b81966e072680
Richa Parmar <richaparmar0109@gmail.com>
2025-12-13 18:33:18 +0530

    Add files via upload

---
+++ Practices/Practices Programs 1 (8.12.25)/areaofcircle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    float PI=3.14;
+    int radius=5;
+    
+    float ans = PI*radius*radius;
+    printf("Area of circle is : %f",ans);
+    
+  
+    
+    getch();
+}
