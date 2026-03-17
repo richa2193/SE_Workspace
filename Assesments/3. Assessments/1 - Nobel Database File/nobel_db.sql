ecb305c3e61b9aaacdac656d536de561b7722f8a
Richa Parmar <richaparmar0109@gmail.com>
2026-03-17 16:47:00 +0530

    commit

---
+++ Assesments/3. Assessments/1 - Nobel Database File/nobel_db.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Mar 17, 2026 at 11:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nobel_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nobel_win`
+--
+
+CREATE TABLE `nobel_win` (
+  `year` int(11) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `winner` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nobel_win`
+--
+
+INSERT INTO `nobel_win` (`year`, `subject`, `winner`, `country`, `category`) VALUES
+(1970, 'Physics', 'Hannes Alfven', 'Sweden', 'Scientist'),
+(1970, 'Physics', 'Louis Neel', 'France', 'Scientist'),
+(1970, 'Chemistry', 'Luis Federico Leloir', 'France', 'Scientist'),
+(1970, 'Physiology', 'Ulf von Euler', 'Sweden', 'Scientist'),
+(1970, 'Physiology', 'Bernard Katz', 'Germany', 'Scientist'),
+(1970, 'Literature', 'Aleksandr Solzhenitsyn', 'Russia', 'Linguist'),
+(1970, 'Economics', 'Paul Samuelson', 'USA', 'Economist'),
+(1970, 'Physiology', 'Julius Axelrod', 'USA', 'Scientist'),
+(1971, 'Physics', 'Dennis Gabor', 'Hungary', 'Scientist'),
+(1971, 'Chemistry', 'Gerhard Herzberg', 'Germany', 'Scientist'),
+(1971, 'Peace', 'Willy Brandt', 'Germany', 'Chancellor'),
+(1971, 'Literature', 'Pablo Neruda', 'chile', 'Linguist'),
+(1971, 'Economics', 'Simon Kuznets', 'Russia', 'Economist'),
+(1978, 'Peace', 'Anwar al-sadat', 'Egypt', 'President'),
+(1978, 'Peace', 'Menachem Begin', 'Israel', 'Prime Minister'),
+(1987, 'chemistry', 'Donald J. Cram', 'USA', 'Scientist'),
+(1987, 'chemistry', 'Jean-Marie Lehn', 'France', 'Scientist'),
+(1987, 'Physiology', 'Susumu Tonegawa', 'Japan', 'Scientist'),
+(1994, 'Economics', 'Reinhard Selten', 'Germany', 'Economist'),
+(1994, 'Peace', 'Yitzhak Rabin', 'Israel', 'Prime Minister'),
+(1987, 'physics', 'Johannes Georg Bednorz', 'Germany', 'Scientist'),
+(1987, 'Literature', 'Joseph Brodsky', 'Russia', 'Linguist'),
+(1987, 'Economics', 'Robert Solow', 'USA', 'Economist'),
+(1994, 'Literature', 'Kenzaburo Oe', 'Japan', 'Linguist');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
